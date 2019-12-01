4f0b94b838f923702c6a85e269c4bbac91620c0f
wangchangshu <wangchangshu@mafengwo.com>
2019-12-01 21:29:51 +0800

    ui体验

---
+++ app/src/main/java/com/wang/materialdesign/Fruit.java
@@ -0,0 +1,23 @@
+package com.wang.materialdesign;
+
+/**
+ * Created by wangchangshu on 2019-12-01.
+ * Describe:
+ */
+public class Fruit {
+    private String name;
+    private int imageId;
+
+    public Fruit(String name, int imageId){
+        this.name = name;
+        this.imageId = imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+}
---
+++ app/src/main/java/com/wang/materialdesign/FruitAdapter.java
@@ -0,0 +1,77 @@
+package com.wang.materialdesign;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+/**
+ * Created by wangchangshu on 2019-12-01.
+ * Describe:
+ */
+public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder> {
+    private Context context;
+    private List<Fruit> mFruitList;
+
+    public FruitAdapter(Context context, List<Fruit> fruitList) {
+        this.context = context;
+        this.mFruitList = fruitList;
+
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        private ImageView fruitImage;
+        private TextView fruitname;
+        private CardView cardView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            cardView = (CardView) itemView;
+            fruitImage = itemView.findViewById(R.id.fruit_image);
+            fruitname = itemView.findViewById(R.id.fruit_name);
+        }
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.fruit_item, parent,false);
+        final ViewHolder holder = new ViewHolder(view);
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = holder.getAdapterPosition();
+                Fruit fruit = mFruitList.get(position);
+                Intent intent = new Intent(context, FruitActivity.class);
+                intent.putExtra(FruitActivity.FRUIT_NAME, fruit.getName());
+                intent.putExtra(FruitActivity.FRUIT_IMAGE_ID, fruit.getImageId());
+                context.startActivity(intent);
+            }
+        });
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Fruit fruit = mFruitList.get(position);
+        holder.fruitname.setText(fruit.getName());
+        Glide.with(context).load(fruit.getImageId()).into(holder.fruitImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mFruitList.size();
+    }
+
+}
